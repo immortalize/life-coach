33d9c19ba616375c5996faca8d80e52a640b84a5
Bekir Eldem <bekir.eldem@gmail.com>
2018-02-27 20:09:55 +0300

    make it better

--- app/Http/Controllers/SleepController.php
+++ app/Http/Controllers/SleepController.php
@@ -50,7 +50,7 @@ class SleepController extends Controller
 
     /**
      * Show the form for creating a new resource.
-     *
+     * 
      * @return \Illuminate\Http\Response
      */
     public function create()
