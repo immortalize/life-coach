1f9b9c0f8abd24573e703b8c93e04aa4fd3d8423
Bekir Eldem <bekir.eldem@gmail.com>
2018-04-05 14:15:20 +0300

    layout fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,10 +55,14 @@
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
+                                    {{ Auth::user()->name }} <span class="caret"></span></a>
                                 <ul class="dropdown-menu" role="menu">
+                                    <li>
+                                        <a href="{{ url('/goals') }}">My Goals</a>
+                                    </li>
+                                    <li>
+                                        <a href="{{ url('/sleep') }}">Sleep Log</a>
+                                    </li>
                                     <li>
                                         <a href="{{ url('/logout') }}"
                                             onclick="event.preventDefault();
@@ -70,13 +74,6 @@
                                             {{ csrf_field() }}
                                         </form>
                                     </li>
-
-                                    <li>
-                                        <a href="{{ url('/sleep') }}">
-                                            Sleep
-                                        </a>
-                                    </li>
-
                                 </ul>
                             </li>
                         @endif
