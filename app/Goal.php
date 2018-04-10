2c80c368b3165b7ad0c094ee21b35aa738f96d0f
Bekir Eldem <bekir.eldem@gmail.com>
2018-04-10 14:11:20 +0300

    fixed the 4th bug

--- app/Goal.php
+++ app/Goal.php
@@ -7,4 +7,37 @@ use Illuminate\Database\Eloquent\Model;
 class Goal extends Model
 {
     //
+    /**
+     * The sub-goals that belong to the goal.
+     */
+    public function sub_goals()
+    {
+        return $this->belongsToMany('App\Goal', 'goal_relations', 'goal_id', 'child_goal_id');
+    }
+
+    /**
+     * The parent goals of the goal.
+     */
+    public function parent_goals()
+    {
+        return $this->belongsToMany('App\Goal', 'goal_relations', 'child_goal_id', 'goal_id');
+    }
+
+    /**
+     * The reasons of the goal.
+     */
+    public function reasons()
+    {
+        return $this->hasMany('App\Reason');
+    }    
+
+    /**
+     * The sub-goals that does not belong to the goal.
+     *
+    public function potential_sub_goals()
+    {
+        return $this->belongsToMany('App\Goal', 'goal_relations', 'goal_id', 'child_goal_id')
+        			->wherePivot('goal_id', '<>' {$this->id});
+    } 
+    */ 
 }
